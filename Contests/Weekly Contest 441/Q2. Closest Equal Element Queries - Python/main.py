5f08ad51eb969d70191568c8fc50065401d4467d
Royut <mohammadzareei36@gmail.com>
2025-03-15 20:44:14 -0700

    Weekly Contest 441 - Q2 - attempt 2

--- Contests/Weekly Contest 441/Q2. Closest Equal Element Queries - Python/main.py
+++ Contests/Weekly Contest 441/Q2. Closest Equal Element Queries - Python/main.py
@@ -15,24 +15,43 @@ class Solution(object):
         ans = []
         for q in queries:
             numToFind = nums[q]
-            # print('to fiund', numToFind, q)
-            if numToFind not in locs or len(locs[numToFind]) == 1:
+            rightIndex = q+1
+            leftIndex = q-1
+            if rightIndex >= len(nums):
+                rightIndex -= len(nums)
+            if leftIndex < 0:
+                leftIndex += len(nums)
+            foundFlag = False
+            # print('q', q, numToFind)
+            while True:
+                # print('indexes', leftIndex, rightIndex)
+                if numToFind == nums[leftIndex] and leftIndex != q:
+                    dist = min(abs(q - leftIndex), abs(len(nums) - abs(q - leftIndex)))
+                    ans.append(dist)
+                    foundFlag = True
+                    break
+                if numToFind == nums[rightIndex] and rightIndex != q:
+                    dist = min(abs(rightIndex - q), abs(len(nums) - abs(q - rightIndex)))
+                    ans.append(dist)
+                    foundFlag = True
+                    break
+                if rightIndex == leftIndex:
+                    break
+                rightIndex += 1
+                leftIndex -= 1
+                if rightIndex >= len(nums):
+                    rightIndex -= len(nums)
+                if leftIndex < 0:
+                    leftIndex += len(nums)
+            # print(foundFlag)
+            if not foundFlag:
                 ans.append(-1)
-            else:
-                # print(locs[numToFind],q)
-                minDist = float('inf')
-                for numFound in locs[numToFind]:
-                    # print(numFound - q)
-                    dist = min(abs(numFound - q), abs(len(nums) - abs(numFound - q)))
-                    if dist < minDist and dist != 0:
-                        minDist = dist
-                ans.append(minDist)
-            # print('ans', ans)
+            # print(ans)
         return ans
 
 
 if __name__ == '__main__':
     print('in main')
-    nums = [1,3,1,4,1,3,2]
-    queries = [0,3,5]
+    nums = [5,20,12,14,12,17,13,7,7,5,20,5,5,5,12,19,6]
+    queries = [3,14,2,0,8,12,16,6,7,11,4,10]
     print(Solution().solveQueries(nums, queries))
