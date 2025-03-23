af007949593b70303d46c7b72e7e5148360dfa63
Royut <mohammadzareei36@gmail.com>
2025-03-22 20:59:12 -0700

    Weekly Contest 442 - Q3 - attempt 2 - time limit

--- Contests/Weekly Contest 442/Q3. Find the Minimum Amount of Time to Brew Potions - Python/main.py
+++ Contests/Weekly Contest 442/Q3. Find the Minimum Amount of Time to Brew Potions - Python/main.py
@@ -8,17 +8,21 @@ class Solution(object):
         table = []
         for i in range(len(mana)):
             temp = [mana[i]*skill[0]]
+            if i > 0:
+                if len(skill) >= 2:
+                    maxTimeDiff = max(min(table[i-1]), table[i-1][1] - temp[0])
+                else:
+                    maxTimeDiff = min(table[i - 1])
             for j in range(1, len(skill)):
                 temp.append(temp[-1] + mana[i]*skill[j])
-            table.append(temp)
-            # print(temp)
+                if i > 0 and j < len(skill) - 1:
+                    if table[i-1][j+1] - temp[j] > maxTimeDiff:
+                        maxTimeDiff = table[i-1][j+1] - temp[j]
+            # if i > 0:
+            #     print(maxTimeDiff)
             if i > 0:
-                maxTimeDiff = min(table[i-1])
-                for j in range(len(skill)-1):
-                    if table[i-1][j+1] - table[i][j] > maxTimeDiff:
-                        maxTimeDiff = table[i-1][j+1] - table[i][j]
                 for j in range(len(skill)):
-                    table[i][j] += maxTimeDiff
-                # print(maxTimeDiff)
+                    temp[j] += maxTimeDiff
+            table.append(temp)
         # print(table)
         return table[-1][-1]
