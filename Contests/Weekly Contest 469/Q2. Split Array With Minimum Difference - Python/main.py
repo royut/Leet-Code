fdc22c19a795acc675170921b659c01bb298499a
Royut <mohammadzareei36@gmail.com>
2025-09-27 20:22:49 -0700

    Weekly Contest 469 - Q2 - attempt

---
+++ Contests/Weekly Contest 469/Q2. Split Array With Minimum Difference - Python/main.py
@@ -0,0 +1,51 @@
+class Solution(object):
+    def splitArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        sumFromStart = 0
+        sumsFromStart = []
+        sumFromEnd = 0
+        sumsFromEnd = []
+        length = len(nums)
+        for i in range(len(nums)):
+            sumFromStart += nums[i]
+            sumsFromStart.append(sumFromStart)
+            sumFromEnd += nums[len(nums) - i - 1]
+            sumsFromEnd.append(sumFromEnd)
+        # print(sumsFromStart)
+        # print(sumsFromEnd)
+
+        increasing = True
+        shuffleCount = 0
+        shuffleIndex = 1
+        for i in range(1, len(nums)):
+            if increasing and nums[i] > nums[i - 1]:
+                pass
+            elif not increasing and nums[i] < nums[i - 1]:
+                pass
+            else:
+                increasing = not increasing
+                shuffleCount += 1
+                shuffleIndex = i
+                if shuffleCount == 2:
+                    return -1
+        # print(shuffleCount)
+        # print(shuffleIndex)
+        print(nums[:shuffleIndex], nums[shuffleIndex:])
+        diff = abs(sumsFromStart[shuffleIndex-1] - sumsFromEnd[length - shuffleIndex - 1])
+        if nums[shuffleIndex-1] > nums[shuffleIndex] and shuffleIndex - 1 != 0:
+            print(nums[:shuffleIndex-1], nums[shuffleIndex-1:])
+            newDiff = abs(sumsFromStart[shuffleIndex-2] - sumsFromEnd[length - shuffleIndex])
+            diff = min(diff, newDiff)
+        return diff
+
+
+if __name__ == "__main__":
+    nums = [1, 2, 4, 3]
+    # nums = [1, 3, 2]
+    # nums = [1, 2, 1]
+    # nums = [4,3]
+    # nums = [2,4]
+    print(Solution().splitArray(nums))
