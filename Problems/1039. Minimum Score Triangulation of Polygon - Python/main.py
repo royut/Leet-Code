124ad9c6fcdd6110ecffb1bfc9c693b2d4f707e3
Royut <mohammadzareei36@gmail.com>
2025-09-29 20:16:38 -0700

    1039 - no lrc cache in leetcode

--- Problems/1039. Minimum Score Triangulation of Polygon - Python/main.py
+++ Problems/1039. Minimum Score Triangulation of Polygon - Python/main.py
@@ -1,3 +1,6 @@
+from functools import lru_cache
+
+
 class Solution(object):
     def minScoreTriangulation(self, values):
         """
@@ -5,35 +8,25 @@ class Solution(object):
         :rtype: int
         """
         n = len(values)
-        if n == 3:
-            return values[0] * values[1] * values[2]
-        minScore = float('inf')
-        for i in range(n):
-            for j in range(n):
-                if i == 0 and j in [n-1, 0, 1]:
-                    continue
-                elif i == n-1 and j in [n-2, n-1, 0]:
-                    continue
-                elif j in [i-1, i, i+1]:
-                    continue
-                else:
-                    # print(i, j)
-                    if i < j:
-                        arr1 = values[i:j+1]
-                        arr2 = values[j:n] + values[0:i+1]
-                    else:
-                        arr2 = values[j:i+1]
-                        arr1 = values[i:n] + values[0:j+1]
-                    # print(arr1, arr2)
-                    arr1Val = self.minScoreTriangulation(arr1)
-                    arr2Val = self.minScoreTriangulation(arr2)
-                    minScore = min(minScore, arr1Val + arr2Val)
-        return minScore
+        @lru_cache(maxsize=None)
+        def rec(x, y):
+            print('start', x, y)
+            if y-x < 2:
+                return 0
+            if y-x == 2:
+                return values[x] * values[x+1] * values[x+2]
+            minScore = float('inf')
+            for i in range(x+1, y):
+                arr1Val = rec(x, i)
+                arr2Val = rec(i, y)
+                minScore = min(minScore, values[i] * values[x] * values[y] + arr1Val + arr2Val)
+            return minScore
+        return rec(0, len(values)-1)
 
 
 if __name__ == "__main__":
     print("in main")
     values = [1, 3, 1, 4, 1, 5]
     # values = [3, 7, 4, 5]
-    values = [3,8,8,5,9,8,2,2,2]
+    # values = [3,8,8,5,9,8,2,2,2]
     print(Solution().minScoreTriangulation(values))
